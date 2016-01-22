24be716ebdfd05435bd35b8115898642c0bfd074
kv113813 <devopsmaster@gmail.com>
2016-01-21 21:11:14 -0800

    Signed-off-by: kv113813 <devopsmaster@gmail.com>

--- src/com/javapapers/jee/AnimalTypeService.java
+++ src/com/javapapers/jee/AnimalTypeService.java
@@ -1,5 +1,5 @@
 package com.javapapers.jee;
-//Evaluation of source tree- changes-1
+//Evaluation of source tree- changes-2
 public class AnimalTypeService {
 	public String animalType(String animal) {
 		String animalType = "";
